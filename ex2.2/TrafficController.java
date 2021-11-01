8de51c6ec1313194b5223c4dbf7af9e6c6b46792
matiarasianipar <77903930+matiarasianipar@users.noreply.github.com>
2021-10-31 23:41:50 -0700

    Add files via upload

---
+++ ex2.2/TrafficController.java
@@ -0,0 +1,47 @@
+public class TrafficController {
+
+    private Integer l2r = 0;
+    private Integer r2l = 0;
+
+    public void enterLeft() {
+        synchronized (this) {
+            while (r2l != 0) {
+                try {
+                    wait ();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            l2r++;
+            notifyAll();
+        }
+    }
+
+    public void enterRight() {
+        synchronized (this) {
+            while (l2r != 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            r2l++;
+            notifyAll();
+        }
+    }
+
+    public void leaveLeft() {
+        synchronized (this) {
+            r2l--;
+            notifyAll();
+        }
+    }
+    public void leaveRight() {
+        synchronized (this) {
+            l2r--;
+            notifyAll();
+        }
+    }
+
+}
